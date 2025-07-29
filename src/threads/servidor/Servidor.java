242ceb74697844db5b3dc4e4efdc99f60a9edb85
mananas <mananas@10.1.2.15>
2025-07-29 12:24:52 +0200

    servidorescuchando

--- src/threads/servidor/Servidor.java
+++ src/threads/servidor/Servidor.java
@@ -1,6 +1,9 @@
 package threads.servidor;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,6 +17,7 @@ public class Servidor {
 
 	public void start() {
 		
+		System.out.println("Servidor esperando conexiones");
 		try (ServerSocket server = new ServerSocket(puerto)){
 			
 			while(true) {
@@ -28,22 +32,38 @@ public class Servidor {
 		}
 	}
 
-private class AtiendeCliente implements Runnable {
+	private class AtiendeCliente implements Runnable {
 	
-	private Socket socket;
-	
-	public AtiendeCliente(Socket socket) {
-		this.socket = socket;
-		new Thread(this).start();
-	}
-
-	@Override
-	public void run() {
+		private Socket socket;
 		
-		
+		public AtiendeCliente(Socket socket) {
+			this.socket = socket;
+			new Thread(this).start();
+		}
+	
+		@Override
+		public void run() {
+			try(PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+					BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+			
+				
+				out.println("Bienvenido al Servidor MUTONTO!!");
+				
+				String linea;
+				while ((linea = in.readLine()) != null) {
+					out.println("SRV: " + linea);
+				}
+				
+			}catch (IOException e) {
+			e.printStackTrace();
+			}
+		}
 	}
 	
-}
+	public static void main(String[] args) {
+		new Servidor(1234).start();
+	}
+	
 }
 
 
